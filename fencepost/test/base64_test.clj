eff9ecef354bea6b678e9bb63edc9db2df7a3962
mersault <mersault@bartok.(none)>
2010-10-27 21:50:06 -0500

    Cleaning a few things up.

--- fencepost/test/base64_test.clj
+++ fencepost/test/base64_test.clj
@@ -1,12 +1,12 @@
+(ns fencepost.test)
+
 (import '(org.apache.commons.codec.binary Base64))
 (import '(org.apache.commons.lang RandomStringUtils))
-;(use '[clojure.contrib.base64 :only (encode)])
-;(use '[fencepost.base64 :only (base64-encode)])
 (use '[clojure.contrib.base64])
 (use '[fencepost.base64])
 
-(def sample-size 10000)
-(def string-size 64)
+(def sample-size 100)
+(def string-size 256)
 
 ; Build up some sample data using commons-lang
 (def sample-data (map #(RandomStringUtils/randomAlphabetic %) (repeat sample-size string-size)))
@@ -16,7 +16,10 @@
 (def codec-base64 (new Base64))
 
 ; Time each run
-(time (map #(.encode codec-base64 %) sample-data))
-(time (map #(clojure.contrib.base64/encode %) sample-data))
-(time (map #(fencepost.base64/base64-encode %) sample-data))
+(println "Commons-codec")
+(time (assert (= (count (map #(.encode codec-base64 (.getBytes %)) sample-data)) sample-size))) 
+(println "clojure-contrib")
+(time (assert (= (count (map #(clojure.contrib.base64/encode-str %) sample-data)) sample-size))) 
+(println "fencepost")
+(time (assert (= (count (map #(fencepost.base64/base64-encode %) sample-data)) sample-size)))
 
---
+++ set_classpath.sh
@@ -0,0 +1,1 @@
+export CLASSPATH="$HOME/Java/commons-codec-1.4/commons-codec-1.4.jar:$HOME/Java/commons-lang-2.5/commons-lang-2.5.jar:$HOME/git/clojure-contrib/src/main/clojure:."
