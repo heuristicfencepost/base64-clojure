af43b33d5e804e96fedb204634b7bb99853a4702
Heuristic Fencepost <h.fencepost@gmail.com>
2010-12-27 23:07:59 -0600

    Some minor refactoring... there's room for optimization here.

--- fencepost/base64_recur.clj
+++ fencepost/base64_recur.clj
@@ -78,23 +78,26 @@
        (let [[char1 char2 char3 char4] (map from-base64-recur (apply str (take 4 currbytes)))
              byte1 (bit-or (bit-shift-left char1 2) (bit-shift-right char2 4))
              byte2 (bit-and 0xff (bit-or (bit-shift-left char2 4) (bit-shift-right char3 2)))
-             byte3 (bit-and 0xff (bit-or (bit-shift-left char3 6) char4))]
-         (str rv (String. (byte-array (map byte [byte1 byte2 byte3]))))
+             byte3 (bit-and 0xff (bit-or (bit-shift-left char3 6) char4))
+             newstr (String. (byte-array (map byte [byte1 byte2 byte3])))]
+         (str rv newstr)
          )
        ; Second byte is the low four bits of byte 2 plus the top four
        ; bits of byte 3
        (re-matches #"[A-Za-z0-9+/]{3}=" (apply str (take 4 currbytes)))
        (let [[char1 char2 char3] (map from-base64-recur (apply str (take 3 currbytes)))
              byte1 (bit-or (bit-shift-left char1 2) (bit-shift-right char2 4))
-             byte2 (bit-and 0xff (bit-or (bit-shift-left char2 4) (bit-shift-right char3 2)))]
-         (str rv (String. (byte-array (map byte [byte1 byte2]))))
+             byte2 (bit-and 0xff (bit-or (bit-shift-left char2 4) (bit-shift-right char3 2)))
+             newstr (String. (byte-array (map byte [byte1 byte2])))]
+         (str rv newstr)
          )
        ; First byte is the low six bits of byte 1 plus the top two
        ; bits of byte 2
        (re-matches #"[A-Za-z0-9+/]{2}==" (apply str (take 4 currbytes)))
        (let [[char1 char2] (map from-base64-recur (apply str (take 2 currbytes)))
-             byte1 (bit-or (bit-shift-left char1 2) (bit-shift-right char2 4))]
-         (str rv (String. (byte-array [(byte byte1)])))
+             byte1 (bit-or (bit-shift-left char1 2) (bit-shift-right char2 4))
+             newstr (String. (byte-array [(byte byte1)]))]
+         (str rv newstr)
          )
        (= samplesize 0) ""
        )
